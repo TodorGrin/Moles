9654946825f26c13e39fde5015a559ec58a58396
Todor <todorhryn@gmail.com>
2020-12-16 13:53:13 +0300

    Alway open fullscreen

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
+    w.openMainMenu();
     w.show();
     return a.exec();
 }
